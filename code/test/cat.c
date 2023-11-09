3f9086f380e3a7b2eb2168345c368b25909585b1
cheesepp <nhattri2904@gmail.com>
2023-11-09 15:19:51 +0700

    done (maybe)

--- code/test/cat.c
+++ code/test/cat.c
@@ -2,14 +2,37 @@
 
 #define CONSOLE_INPUT 0
 #define CONSOLE_OUTPUT 1
+#define MAX_LENGTH 32
 
 int main() {
-    int id;
     char buffer[256];
-
-    id = Open("text.txt", 0);
-    Read(buffer,256, id);
-    Write(buffer,len(buffer),CONSOLE_OUTPUT);
-    Close(id);
+    int openFileId;
+	int fileSize;
+	char c;
+	char fileName[MAX_LENGTH];
+	int i; //Index for loop
+	PrintString("\n\t\t\tHIEN THI NOI DUNG FILE\n\n");
+	PrintString("Nhap vao ten file can doc: ");
+	
+	// read string file name
+	ReadString(fileName, MAX_LENGTH);
+	
+    // open file
+	openFileId = Open(fileName, 1);
+	
+	if (openFileId != -1) 
+	{
+		// read and save to buffer
+		Read(buffer,256, openFileId);
+        // print string
+		PrintString("Noi dung file:\n");
+		PrintString(buffer);
+		Close(openFileId); // Close file
+	}
+	else
+	{
+		PrintString("Khong the mo file!\n\n");
+	}
+	
     Halt();
 }
